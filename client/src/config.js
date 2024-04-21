cf11e07029aa920162bf2552679d4091fdd5a0c8
Om Nigam <om2021064@akgec.ac.in>
2024-04-22 00:15:24 +0530

    adding new api

---
+++ client/src/config.js
@@ -0,0 +1,2 @@
+export const BASE_URL = "http://localhost:8080";
+// export const BASE_URL = "https://dall-e-04o3.onrender.com";
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,51 +1,62 @@
 import express from "express";
-import * as dotenv from 'dotenv'
-import { v2 as cloudinary } from 'cloudinary'
+import * as dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
 
-import Post from '../mongodb/models/post.js'
+import Post from "../mongodb/models/post.js";
 
-dotenv.config()
+dotenv.config();
 
-const router = express.Router()
+const router = express.Router();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 //Get all post
-router.route('/').get(async (req, res) => {
-    try {
-      const posts = await Post.find({});
-      res.status(200).json({ success: true, data: posts });
-    } catch (err) {
-      res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });
-    }
-  });
-  
+router.route("/").get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ success: true, data: posts });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Fetching posts failed, please try again",
+    });
+  }
+});
 
-  //create post route
-  router.route('/').post(async (req, res) => {
-    try {
-      const { name, prompt, photo } = req.body;
-      const photoUrl = await cloudinary.uploader.upload(photo);
-  
+//create post route
+router.route("/").post(async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
 
-      //creating new post in database
-      const newPost = await Post.create({
-        name,
-        prompt,
-        photo: photoUrl.url,
-      });
-  
-      res.status(200).json({ success: true, data: newPost });
-    } catch (err) {
-      res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
-    }
-  });
+    const photoUrl = await cloudinary.uploader.upload(
+      photo,
+      function (error, result) {
+        if (error) {
+          console.log(error);
+        }
+        console.log(result);
+      }
+    );
 
-  
-  export default router;
+    //creating new post in database
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
 
- 
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: "Unable to create a post, please try again",
+    });
+  }
+});
+
+export default router;
